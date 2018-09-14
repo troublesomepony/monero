a55d8278172bc32536a286f4fd8ed33bb9798c81
troublesomepony <troublesomepony@gmail.com>
2018-09-14 22:02:13 +0300

    Advanced wallet.sh script to run wallet & daemon from project root instead of cd`ing to build/

--- wallet.sh
+++ wallet.sh
@@ -1,4 +1,7 @@
-export type=release
+export default_type="release"
+export release_type=${2:-$default_type}
+export default_wallet="./wallets/default"
+export wallet_file=${1:-$default_wallet}.bin
 
-./build/${type}/bin/monero-wallet-cli --wallet-file=./wallets/MRX.bin
-
+echo "Using wallet file ${wallet_file} and ${release_type} type"
+./build/${release_type}/bin/monero-wallet-cli --wallet-file=${wallet_file}
